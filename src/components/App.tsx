bda54f0daf4a894a73e20977272da74e3e4ed561
Alex Castillo <alex.castillo@razortracking.com>
2025-03-17 21:59:35 -0600

    set to template

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import Avatar from 'components/Avatar'
+import logo from 'assets/logo.svg'
+
+const randoms = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7]
+]
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <button>
+        button 1
+      </button>
+      <button>
+        button 2
+      </button>
+      <button>
+        button 3
+      </button>
+    </div>
+  )
+}
+
+export default App
